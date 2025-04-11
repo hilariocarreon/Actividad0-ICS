fb3ee2f8ec1f9a74759cad34ea6823bb414416a4
Hilario Carreon <hilariocarreon03@gmail.com>
2025-04-11 16:52:45 -0300

    [Parte 4.3] Reemplaza vistas por vistas genéricas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,28 +1,31 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
-from .models import Question, Choice
-from django.http import Http404
 from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
 
 
-def index(request):
-    # Obtiene las 5 preguntas más recientes ordenadas por fecha de publicación descendente
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    # Renderiza la plantilla 'index.html' con la lista de preguntas
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    # Intenta obtener la pregunta según el ID, o lanza un error 404 si no existe
-    question = get_object_or_404(Question, pk=question_id)
-    # Renderiza la plantilla 'detail.html' con los datos de la pregunta
-    return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
 def vote(request, question_id):
     # Intenta obtener la pregunta correspondiente al ID
     question = get_object_or_404(Question, pk=question_id)
